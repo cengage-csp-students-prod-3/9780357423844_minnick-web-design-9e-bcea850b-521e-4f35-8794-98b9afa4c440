5a43fa520436f6c8004a59dad70c71b280bc5021
BadNews94 <nick.threeman@gmail.com>
2026-04-10 22:31:26 +0000

    Testing Fri Apr 10 2026 22:31:26 GMT+0000 (Coordinated Universal Time)

--- chapter10/ex02/student/rescue/scripts/script.js
+++ chapter10/ex02/student/rescue/scripts/script.js
@@ -4,6 +4,10 @@ File Name: script.js
 Date: 04/10/2026
 */
 
+//Global variables
+var answer = document.querySelector("#answer p");
+var heading = document.querySelector("#answer h2");
+
 //Hamburger menu function
 function menu() {
     var navlinks = document.getElementById("nav-links");
