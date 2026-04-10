43afd6973c58409d054ad55c1fb8f22d75efd710
BadNews94 <nick.threeman@gmail.com>
2026-04-10 22:20:06 +0000

    Testing Fri Apr 10 2026 22:20:06 GMT+0000 (Coordinated Universal Time)

--- chapter10/ex02/student/rescue/scripts/script.js
+++ chapter10/ex02/student/rescue/scripts/script.js
@@ -1,5 +1,18 @@
 /*
 Author: Nicholas Brantley
-File Name: scripts.js
+File Name: script.js
 Date: 04/10/2026
 */
+
+//Hamburger menu function
+function menu() {
+    var navlinks = document.getElementById("nav-links");
+		var menuicon = document.getElementById("icon");
+		if (navlinks.style.display === "block") {
+		    navlinks.style.display = "none";
+				menuicon.style.color = "#2a1f14";
+		} else {
+		    navlinks.style.display = "block";
+				menuicon.style.color = "#f6eee4";
+		}
+}
